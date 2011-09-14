12bf41797413b0c7e956adc48043487e5706f088
Jakub Wolny <j.wolny@101studio.pl>
2011-09-14 14:05:38 +0200

    0.3 version - logout support

--- js/wykop.js
+++ js/wykop.js
@@ -1,12 +1,26 @@
-var notifications = document.querySelectorAll('#content .entry-list-notification li');
-var result = [];
-for(var i = 0; i < notifications.length; i++){
-    result.push({
-        'new': notifications[i].className == 'new',
-        'html': notifications[i].innerHTML,
-    });
+var data = {
+    logged: !document.getElementById('quick-login'),
+    news: 0
+};
+
+
+if(document.querySelector('#content .entry-list-notification')){
+    var notifications = document.querySelectorAll('#content .entry-list-notification li');
+    var result = [];
+    for(var i = 0; i < notifications.length; i++){
+        result.push({
+            'new': notifications[i].className == 'new',
+            'html': notifications[i].innerHTML,
+        });
+    }
+    data.notifications = result;
 }
 
-chrome.extension.sendRequest({
-    notifications: result
-});
+
+var news = document.querySelectorAll('#sub-list li.right');
+if(news.length == 2){
+    var match = news[1].textContent.match(/Masz nowe powiadomienia \((\d+)\)/);
+    data.news = match[0];
+}
+
+chrome.extension.sendRequest(data);
