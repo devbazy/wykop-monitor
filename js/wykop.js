4df69dd4a56e934e43dea70d95d779c2098473fa
Jakub Wolny <j.wolny@101studio.pl>
2011-09-17 19:22:57 +0200

    emulation of news notification on page

--- js/wykop.js
+++ js/wykop.js
@@ -29,4 +29,29 @@ if(news.length == 2){
     data.news = match[0];
 }
 
-chrome.extension.sendRequest(data);
+chrome.extension.sendRequest(data, function(answer){
+    if(answer.news){
+        var list = document.getElementById('sub-list').getElementsByTagName('ul')[0]; // #sublist ul
+        if(list){
+            var li = document.createElement('li');    
+            li.className = 'right';
+            li.innerHTML = '<div><a href="http://www.wykop.pl/moj/powiadomienia/" title="Zobacz nowe powiadomienia">Masz nowe powiadomienia (' +  answer.news + ')</a></div>';
+            list.insertBefore(li, list.childNodes[0]);
+    
+            li = document.createElement('li');
+            li.className = 'right check';
+            li.innerHTML = '<div><a href="http://www.wykop.pl/moj/powiadomienia/" title="Zobacz nowe powiadomienia">Sprawdź</a></div>';
+            list.insertBefore(li, list.childNodes[0]);
+        }
+    }
+    
+    if(answer.newstemp){
+        if(document.querySelector('#content .entry-list-notification') !== null){
+            var notifications = document.querySelectorAll('#content .entry-list-notification li');
+            for(var i = 0; i < answer.newstemp; i++){
+                notifications[i].className = 'new';
+            }
+        }
+    }
+});
+
