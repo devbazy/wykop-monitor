28b5dcb502b140763a84a2872e9fb826d5008c08
Jakub Wolny <j.wolny@101studio.pl>
2011-09-23 03:26:19 +0200

    wykop3 working prototype

--- js/wykop.js
+++ js/wykop.js
@@ -3,31 +3,21 @@ var data = {
 };
 
 if(document.getElementById('header-container')){
-    if(document.getElementById('quick-login') !== null){
-        data.logged = false;
-    }else if(document.querySelector('.user-panel') !== null){
-        data.logged = true;
-    }
+    data.logged = /avatar/.test(document.querySelector('#header-container div.quickpoint').className);
 }
 
-if(document.querySelector('#content .entry-list-notification') !== null){
-    var notifications = document.querySelectorAll('#content .entry-list-notification li');
-    var result = [];
-    for(var i = 0; i < notifications.length; i++){
-        result.push({
-            'new': notifications[i].className == 'new',
-            'html': notifications[i].innerHTML,
-        });
-    }
-    data.notifications = result;
-}
+//if(document.querySelector('#content .entry-list-notification') !== null){
+//    var notifications = document.querySelectorAll('#content .entry-list-notification li');
+//    var result = [];
+//    for(var i = 0; i < notifications.length; i++){
+//        result.push({
+//            'new': notifications[i].className == 'new',
+//            'html': notifications[i].innerHTML,
+//        });
+//    }
+//    data.notifications = result;
+//}
 
-
-var news = document.querySelectorAll('#sub-list li.right');
-if(news.length == 2){
-    var match = news[1].textContent.match(/Masz nowe powiadomienia \((\d+)\)/);
-    data.news = match[0];
-}
 
 chrome.extension.sendRequest(data, function(answer){
     if(answer.news){
